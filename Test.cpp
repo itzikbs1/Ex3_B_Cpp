86d7231ca3c3b2793b6689284db8594e4f3496d1
itzik <itzik3877@gmail.com>
2022-04-10 13:30:09 +0300

    fix test

--- Test.cpp
+++ Test.cpp
@@ -30,10 +30,10 @@ TEST_CASE("Case: Invalid multiplication and amount"){
     {
     vector<double> arr1 = {i, i+1, i+2, i+3};//2x2
     vector<double> arr2 = {i,i,i,i,i,i,i,i,i};//3x3
-    vector<double> arr = {i};//1x3
+    vector<double> arr = {i};//1x1
     Matrix mat1(arr1,2,2);
     Matrix mat2(arr2,3,3);
-    Matrix mat(arr,1,3);
+    Matrix mat(arr,1,1);
     CHECK_THROWS(mat1*mat2);
     CHECK_THROWS(mat2*mat1);
     CHECK_THROWS(mat2+mat1);  
@@ -70,7 +70,23 @@ TEST_CASE("Case: Invalid Subtraction matrix"){
 TEST_CASE("matrix multiplication"){
     vector<double> identity_3 = {1,0,0,0,1,0,0,0,1};//3x3
     Matrix mat_identity_3(identity_3,3,3);
-    
+    vector<double> vec1 = {1,1,1,1,1,1,1,1,1};//3x3
+    vector<double> arr = {2,1,1,1,2,1,1,1,2};//3x3   arr = identity_3 + vec1
+    vector<double> vec2 = {1,0,0,0,0,1,0,0,0,0,1,0};//3x4
+    vector<double> identity_4 = {1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1};//4x4
+    vector<double> _vec_ = {1,1,1,1,1,1,1,1,1,1,1,1};//4x3
+    Matrix _mat_(_vec_, 4, 3);
+    Matrix mat1(vec1, 3,3);
+    Matrix mat_arr(arr,3,3);
+    Matrix mat2(vec2,3,4);
+    Matrix mat_identity_4(identity_4,4,4);
+
+    CHECK(((mat1*mat_identity_3) == mat1));
+    CHECK(((mat_identity_3*mat1) == mat1));
+    CHECK(((mat2*mat_identity_4) == mat2));
+    CHECK(((mat1+mat_identity_3) == mat_arr));
+    CHECK(((mat2*_mat_) == mat1));
+
     for (double i = 1; i < 500; i++)
     {
         int random = rand() % 1000;
@@ -90,25 +106,12 @@ TEST_CASE("matrix multiplication"){
 
 
 TEST_CASE("Comparisons between matrices"){
-    vector<double> identity_3 = {1,0,0,0,1,0,0,0,1};//3x3
-    vector<double> vec1 = {1,1,1,1,1,1,1,1,1};//3x3
-    vector<double> arr = {2,1,1,1,2,1,1,1,2};//3x3   arr = identity_3 + vec1
-    vector<double> vec2 = {1,0,0,0,0,1,0,0,0,0,1,0};//3x4
-    vector<double> identity_4 = {1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1};//4x4
-    Matrix mat_identity_3(identity_3,3,3);
-    Matrix mat1(vec1, 3,3);
-    Matrix mat_arr(arr,3,3);
-    Matrix mat2(vec2,3,4);
-    Matrix mat_identity_4(identity_4,4,4);
-    CHECK(((mat1*mat_identity_3) == mat1));
-    CHECK(((mat_identity_3*mat1) == mat1));
-    CHECK(((mat2*mat_identity_4) == mat2));
-    CHECK(((mat1+mat_identity_3) == mat_arr));
+
     for (double i = 1; i < 500; i++)
     {
         vector<double> vec1 = {i,i,i,i};//2x2
         vector<double> identity_2 = {i,0,0,i};//2x2
-        vector<double> vec_ = {i,i,i,i,i,i};//3x3
+        vector<double> vec_ = {i,i,i,i,i,i,i,i,i};//3x3
         Matrix mat(vec1,2,2);
         Matrix mat_identity_2(identity_2,2,2);
         Matrix mat_(vec_,3,3);
@@ -151,7 +154,6 @@ TEST_CASE("Subtraction and amount"){
 
         CHECK(((+matrix) != matrix_sub));
         CHECK(((matrix - mat_identity_3) == matrix_sub));
-        CHECK(((matrix -= k) == matrix_));
     }
 }
 TEST_CASE("CHECK ++ and -- operators"){
